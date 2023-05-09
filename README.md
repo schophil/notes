73352561d80f88cdb22cbf79e7029489e54ca7da
Philippe Schottey <schophil@icloud.com>
2023-05-09 09:33:53 +0200

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# recora
+# My personal notes
+
+- [ChipIT](./doc/chipit.md)
+- [Java](./doc/java.md)
+- [Spring](./doc/spring.md)
+
---
+++ notes.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  note_name="tmp"
+else
+  note_name=$1
+fi
+
+createNote() {
+  note_file="$NOTES_HOME/$note_name.md"
+  if [[ -e $note_file ]]; then
+    $EDITOR $note_file
+  else
+    # creating new note
+    touch $note_file
+    $EDITOR $note_file
+  fi
+}
+
+syncNotes() {
+  cd $NOTES_HOME
+  git add -A
+  git commit -m "feat: sync notes"
+  git push
+}
+
+searchNote() {
+  hits=( `grep -l -r $1 $NOTES_HOME/*.md` )
+  for i in $hits; do
+    fileName=${i##*/}
+    echo "${fileName%.md}"
+  done
+}
+
+if [[ "$note_name" == "sync" ]]; then
+  syncNotes
+elif [[ "$note_name" == "search" ]]; then
+  searchNote $2
+else
+  createNote
+fi
+
