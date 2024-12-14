9fc43c172ddd049258524e57841e282982967f59
Philippe Schottey <schophil@icloud.com>
2024-12-14 07:49:44 +0100

    feat: ignore empty notes

--- notes.sh
+++ notes.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-  note_name="tmp"
-else
-  note_name=$1
-fi
-
-createTempNote() {
+createNote() {
   # create temporary file
   note_file=$(mktemp "/tmp/notes-XXXXX.md")
 
   # start editor
   echo "editing $note_file"
   $NOTES_EDITOR $note_file
-
+  
   # after editing
-  # retrieve the page
-  title=$(head -n 1 ${note_file})
-  page="${title//#/}"
-  page="${page// /}"
-  echo "page: $page"
-
-  # creatte or update page file
-  page_file="$NOTES_HOME/$page.md"
-  if [[ ! -e $page_file ]]; then
-    echo "creating new page file"
-    touch $page_file
-    echo -e "# $page\n" >> $page_file
+  if [[ -s $note_file ]]; then
+    # retrieve the page
+    title=$(head -n 1 ${note_file})
+    page="${title//#/}"
+    page="${page// /}"
+    echo "page: $page"
+    # create or update page file
+    page_file="$NOTES_HOME/$page.md"
+    if [[ ! -e $page_file ]]; then
+      echo "creating new page file"
+      touch $page_file
+      echo -e "# $page\n" >> $page_file
+    fi
+    # create section for current date
+    date=$(date -I)
+    echo -e "## $date\n" >> $page_file
+    # append new note under section
+    cat $note_file | grep -v -x "$title" >> $page_file
+  else
+    echo "ignoring empty note"
   fi
-
-  # create section for current date
-  date=$(date -I)
-  echo -e "## $date\n" >> $page_file
-
-  # append new note under section
-  cat $note_file | grep -v -x "$title" >> $page_file
 }
 
 listNotes() {
   if [[ -n "$1" ]]; then
     for i in $NOTES_HOME/$1*; do
       fileName=${i##*/}
-      echo "${fileName%.md}"
+      echo "- ${fileName%.md}"
     done
   else
     for i in $NOTES_HOME/*; do
       fileName=${i##*/}
-      echo "${fileName%.md}"
+      echo "- ${fileName%.md}"
     done
   fi
 }
@@ -59,11 +54,11 @@ searchNote() {
   done
 }
 
-if [[ "$note_name" == "search" ]]; then
+if [[ -z "$1" ]]; then
+  createNote
+elif [[ "$1" == "search" ]]; then
   searchNote $2
-elif [[ "$note_name" == "list" ]]; then
+elif [[ "$1" == "list" ]]; then
   listNotes $2
-else
-  createTempNote
 fi
 
